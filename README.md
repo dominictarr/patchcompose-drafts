c86750c048d8589af05ef125c184ce9019898b0f
Dominic Tarr <dominic.tarr@gmail.com>
2017-12-11 14:16:18 +1300

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# patchcompose-drafts
+
+save/load pathless drafts in localStorage.
+
+this hooks into `patchcompose-insert`,
+that interface was intended for inserting links,
+but also works for drafts because it gives a handle
+to the textarea. The draft is saved under the
+name of the page that the composer sits on.
+
+There maybe some stuff to work out for drafts
+on channels or with private recipients...
+but we'll cross that brigde when we come to it.
+
+## License
+
+MIT
+
+
---
+++ index.js
@@ -0,0 +1,40 @@
+var h = require('hyperscript')
+
+exports.gives = {
+  compose: {insert: true}
+}
+
+exports.create = function (api) {
+  return { compose: { insert: function (ta, meta, context) {
+    if(!context) return
+    var timer
+    var button = h('button', 'save', {onclick: onSave})
+
+    function onSave () {
+      clearTimeout(timer)
+      localStorage[context.path] = ta.value
+      button.textContent = 'saved!'
+    }
+
+    //load from drafts, if defined.
+    if(localStorage[context.path])
+      ta.value = localStorage[context.path]
+
+    ta.oninput = function () {
+      clearTimeout(timer)
+      if(ta.value == '') {
+        delete localStorage[context.path]
+        button.disabled = true
+        return
+      }
+      button.disabled = false
+      button.textContent = 'save'
+      timer = setTimeout(onSave, 3000)
+    }
+
+    return button
+  }}}
+}
+
+
+
